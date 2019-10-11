409f5b72e8a9991cc67ea54bac43ce95b219d9be
aqiang <362115359@qq.com>
2019-10-11 14:08:01 +0800

    'init'

---
+++ ServerConsole/ServerConsole.cpp
@@ -0,0 +1,26 @@
+﻿// ServerConsole.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "HttpServer.h"
+#include "mongoose.h"
+
+bool handle_fun1(std::string url, std::string body, mg_connection *c, OnRspCallback rsp_callback)
+{
+	// do sth
+	std::cout << "handle fun1" << std::endl;
+	std::cout << "url: " << url << std::endl;
+	std::cout << "body: " << body << std::endl;
+	rsp_callback(c, "{name:'gaofeng'}");
+	return true;
+}
+
+int main()
+{
+	std::string port = "7999";
+	auto http_server = std::shared_ptr<HttpServer>(new HttpServer);
+	http_server->Init(port);
+	// add handler
+	http_server->AddHandler("/api/fun1", handle_fun1);
+	http_server->Start();
+}
